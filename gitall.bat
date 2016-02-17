4c4c0ce85690e2ee31d7710afbd1d81595978b64
ilivanoff <ivanov.ilya.alex@gmail.com>
2016-02-18 01:20:51 +0300

    submodules

--- gitall.bat
+++ gitall.bat
@@ -1,7 +1,10 @@
 @echo off
 
-git submodule init
-git submodule update
+rem git pull
+rem git submodule init
+
+git submodule update --init
+git submodule foreach git checkout master
 git submodule foreach git pull origin master
 
 rem Если передан какой-либо параметр, то не ожидаем
