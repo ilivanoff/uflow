b7b6839b091af98f5d6f3cda0a4ec7e46b6dae50
ilivanoff <azazello85@mail.ru>
2016-02-19 00:47:48 +0300

    crop.sql

--- db/build/schemas/crop_test.sql
+++ db/build/schemas/crop_test.sql
@@ -429,11 +429,10 @@ VALUES (100,
  */
 /*
 Created: 14.08.2010
-Modified: 18.02.2016
+Modified: 19.02.2016
 Model: MySQL 5.1
 Database: MySQL 5.1
 */
-
 
 -- Create tables section -------------------------------------------------
 
@@ -442,32 +441,23 @@ Database: MySQL 5.1
 CREATE TABLE crop_cell
 (
   id_cell Int UNSIGNED NOT NULL AUTO_INCREMENT
- COMMENT 'Код ячейки',
-  id_user Int UNSIGNED
- COMMENT 'Пользователь',
-  n_cell Int UNSIGNED NOT NULL
- COMMENT 'Номер ячейки',
+  COMMENT 'Код ячейки',
+  id_user Int UNSIGNED,
   dt_event Int UNSIGNED NOT NULL
- COMMENT 'Дата создания ячейки',
+  COMMENT 'Дата привязки ячейки',
   v_text Text
- COMMENT 'Текст сообщения'
+  COMMENT 'Сообщение',
+  v_temp Varchar(30)
+  COMMENT 'Директория, в которой схарятся временные файлы',
+ PRIMARY KEY (id_cell)
 )
- DEFAULT CHARACTER SET utf8
- COLLATE utf8_general_ci
-;
-
-CREATE INDEX IX_Relationship1 ON crop_cell (id_user)
-;
-
-ALTER TABLE crop_cell ADD  PRIMARY KEY (id_cell)
-;
-
-ALTER TABLE crop_cell ADD UNIQUE n_cell (n_cell)
+  DEFAULT CHARACTER SET utf8
+  COLLATE utf8_general_ci
 ;
 
 -- Create relationships section ------------------------------------------------- 
 
-ALTER TABLE crop_cell ADD CONSTRAINT Relationship1 FOREIGN KEY (id_user) REFERENCES users (id_user) ON DELETE RESTRICT ON UPDATE RESTRICT
+ALTER TABLE crop_cell ADD CONSTRAINT Relationship1 FOREIGN KEY (id_user) REFERENCES users (id_user) ON DELETE NO ACTION ON UPDATE NO ACTION
 ;
 
 /*
