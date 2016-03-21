7eda260da8af40f4b5a3d3bb312301a49f900b64
ilivanoff <azazello85@mail.ru>
2016-03-21 19:32:06 +0300

    crop addons

--- www/ps-addon/folded/basics/index/index.js
+++ www/ps-addon/folded/basics/index/index.js
@@ -22,7 +22,7 @@ $(function () {
 
     // # 1.
     function MosaicMapController() {
-        var $div = null;
+        var $div = null, onHideTimer = null;
 
         var extractCellId = function ($item) {
             var cellId = $item.data('c');
@@ -56,17 +56,30 @@ $(function () {
             }
         }
 
+        var onBubbleHover = function () {
+            onHideTimer.stop();
+        }
+
         var onHide = function () {
             if ($div) {
                 $div.remove();
                 $div = null;
             }
+            if (onHideTimer) {
+                onHideTimer.stop();
+            }
         }
 
-        var onShow = function (e, $item) {
+        onHideTimer = new PsTimerAdapter(onHide, 10);
+
+        var onHideDeferred = function () {
+            onHideTimer.start();
+        }
+
+        var onShow = function (e, $target) {
             onHide();
 
-            var cellId = extractCellId($item);
+            var cellId = extractCellId($target);
             if (!PsIs.integer(cellId))
                 return;//---
 
@@ -78,38 +91,101 @@ $(function () {
 
             $div = $('<div>').addClass('cell-view popup');
 
+            //Ячейка забанена? Показываем окошко с информацией и выходим
             if (obj.b) {
-                $div.append($('<div>').addClass('banned').append(crIMG('/i/gun/barrel.png')).append('&nbsp;Ячейка #' + cellId + ' забанена'));
-            } else {
-                var src = '/c/' + cellId + '/big.png';
-                var $img = $('<img>').addClass('progress').attr('src', CONST.IMG_LOADING_PROGRESS);
-                $div.append($img).data('cell', cellId);
-                var $content = $('<div>').addClass('cell-content').appendTo($div);
-                //Дата
-                $content.append($('<div>').addClass('date').text(obj.d))
-                //Текст
-                if (obj.t) {
-                    $content.append($('<div>').addClass('text').html(obj.h ? obj.t : obj.t.htmlEntities()));
-                }
-                //Автор
-                if (obj.a) {
-                    $content.append($('<div>').addClass('auth').text(obj.a));
-                }
-
-                PsResources.getImgSize(src, function (wh) {
-                    $img.attr('src', wh ? src : '/i/blank.png').removeClass('progress');
-                });
+                $div.addClass('banned').append(crIMG('/i/gun/barrel.png')).append('&nbsp;Ячейка #' + cellId + ' забанена').appendTo('body').calculatePosition(e, 3, 3);
+                return;//---
             }
+
+            var src = '/c/' + cellId + '/big.png';
+            var $img = $('<img>').addClass('progress').attr('src', CONST.IMG_LOADING_PROGRESS).click(onHide);
+            $div.append($img).data('cell', cellId);
+            var $content = $('<div>').addClass('cell-content').appendTo($div);
+            //Дата
+            $content.append($('<div>').addClass('date').text(obj.d))
+            //Текст
+            if (obj.t) {
+                $content.append($('<div>').addClass('text').html(obj.h ? obj.t : obj.t.htmlEntities()));
+            }
+            //Автор
+            if (obj.a) {
+                $content.append($('<div>').addClass('auth').text(obj.a));
+            }
+
+            PsResources.getImgSize(src, function (wh) {
+                $img.attr('src', wh ? src : '/i/blank.png').removeClass('progress');
+            });
 
             $div.append($('<div>').addClass('clearall'));
             $div = CropUtils.prepareCellView(cellId, $div);
             $div.appendTo('body');//.width($div.width());
 
+            //onUpdate(e);
+
+            function attach() {
+                var $targetOffset = $target.offset();
+
+                var cellTop = $targetOffset.top;
+                var cellLeft = $targetOffset.left;
+
+                if ($target.is('area')) {
+                    var wallOffs = CropCore.$wall.offset();
+                    cellLeft = wallOffs.left + 1 * $target.attr('coords').split(',')[0];
+                }
+
+                cellTop = Math.round(cellTop);
+                cellLeft = Math.round(cellLeft);
+
+                var offsetX = -10, offsetY = -10;
+                var trgtWidth = 60, trgtHeight = 60; //Ширина и высота элемента, на который наведён курсор (для !byEvent)
+
+                var pageX = cellLeft + trgtWidth;
+                var pageY = cellTop + trgtHeight; //Точка, относительно которой необходим показ
+
+                var divWidth = $div.psOuterWidth(true);
+                var divHeight = $div.outerHeight(true);
+                var winWidth = $(window).width();
+                var winHeight = $(window).height();
+                var winLeft = $(window).scrollLeft();
+                var winTop = $(window).scrollTop();
+
+                var left = pageX + offsetX; //Факчитеское расстояние до точки показа по горизонтали (с учётом сдвига)
+                if (left + divWidth > winLeft + winWidth) {
+                    var newLeft = left - 2 * offsetX - divWidth - trgtWidth;
+                    if (newLeft < winLeft) {
+                        //Выяснилось, что новая позиция левого края также не помещается в экран
+                        //Вычислим ширину видимой области для обоих положений и сравним их.
+                        //Если в старом варианте (left) видно больше, чем в новом, то оставим старый вариант.
+                        var xbounds1 = PsMath.bounds(left, left + divWidth, winLeft, winLeft + winWidth);
+                        var xvis1 = xbounds1[1] - xbounds1[0];
+                        var xbounds2 = PsMath.bounds(newLeft, newLeft + divWidth, winLeft, winLeft + winWidth);
+                        var xvis2 = xbounds2[1] - xbounds2[0];
+                        if (xvis1 < xvis2) {
+                            left = newLeft;
+                        }
+                    } else {
+                        left = newLeft;
+                    }
+                }
+
+                var top = pageY + offsetY; //Факчитеское расстояние от верха страницы до курсора по вертигаки (с учётом сдвига)
+                if ((top + divHeight > winTop + winHeight) && (top > divHeight)) { //Если при показе элемента вверх он также не влезает, то покажем его вниз
+                    top = top - 2 * offsetY - divHeight - trgtHeight;
+                }
+
+                //Вычислим угол, которым bubble прикреплён к ячейку
+                var xPos = left > cellLeft ? 'l' : 'r';
+                var yPos = top > cellTop ? 'u' : 'b';
+
+                $div.css('left', left).css('top', top).addClass(xPos + yPos);
+            }
+
+            attach();
             onUpdate(e);
         }
 
         var onUpdate = function (e) {
-            if ($div) {
+            if ($div && $div.is('.banned')) {
                 $div.calculatePosition(e, 3, 3);
             }
         }
@@ -121,15 +197,22 @@ $(function () {
                 item: 'map area, img:not(a>img)',
                 mouseenter: onShow,
                 mousemove: onUpdate,
-                mouseleave: onHide,
+                mouseleave: onHideDeferred,
                 click: onClick
+            });
+
+            PsJquery.on({
+                parent: 'body',
+                item: '.cell-view',
+                mouseenter: onBubbleHover,
+                mouseleave: onHide
             });
         } else {
             onShow({
                 pageX: $('.wall')[0].offsetLeft,
                 pageY: 150
             },
-            $('.wall img:first'));
+                    $('.wall img:first'));
         }
     }
     // # 1.
@@ -229,31 +312,31 @@ $(function () {
                 ctxt: this,
                 y: this.getLastY()
             },
-            function (ok) {
-                var $box = $(ok);
-                var $images = $box.find("img[src^='/']");
-                if (!$images.isEmptySet()) {
-                    $box.hide();
+                    function (ok) {
+                        var $box = $(ok);
+                        var $images = $box.find("img[src^='/']");
+                        if (!$images.isEmptySet()) {
+                            $box.hide();
 
-                    var allImgsLoaded = PsUtil.once(function () {
-                        //Если уже всё загружено - прячем кнопку и отписываемся от скрола
-                        if (!this.canPreload()) {
-                            CropCore.$preload.hide();
-                            PsScroll.unbindWndScrolledBottom(this.doPreloadScroll, this);
+                            var allImgsLoaded = PsUtil.once(function () {
+                                //Если уже всё загружено - прячем кнопку и отписываемся от скрола
+                                if (!this.canPreload()) {
+                                    CropCore.$preload.hide();
+                                    PsScroll.unbindWndScrolledBottom(this.doPreloadScroll, this);
+                                }
+                                //Показываем загруженный блок
+                                $box.show();
+                                //Снимаем состояние предзагрузки
+                                preloadingDone();
+                            }, this);
+
+                            PsResources.onAllImagesLoaded($images, allImgsLoaded);
                         }
-                        //Показываем загруженный блок
-                        $box.show();
-                        //Снимаем состояние предзагрузки
-                        preloadingDone();
-                    }, this);
 
-                    PsResources.onAllImagesLoaded($images, allImgsLoaded);
-                }
+                        CropCore.$wall.append($box);
 
-                CropCore.$wall.append($box);
-
-                return true;
-            }, 'Загрузка стены', function (ok) {
+                        return true;
+                    }, 'Загрузка стены', function (ok) {
                 if (!ok) {
                     //Снимаем состояние предзагрузки
                     preloadingDone();
